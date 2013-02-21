b9dcb086ec19fd87eba7ecdbd49a34b7c9eac604
ekarpov <karpov@klocwork.com>
2013-02-21 12:26:00 -0500

    Solved.

---
+++ python/fibd.py
@@ -0,0 +1,24 @@
+cache = {}
+
+def fib(n, m):
+    if n <= 0:
+        return [0] * m
+    if n == 1:
+        return [1] + [0] * (m - 1)
+    if n == 2:
+        return [0, 1] + [0] * (m - 2)
+    if n in cache:
+        return cache[n]
+    p = fib(n - 1, m)
+    res = [0] * m
+    for i in range(1, m):
+        res[0] += p[i]
+        if i < m - 1:
+            res[i + 1] = p[i]
+    res[1] = p[0]
+    cache[n] = res
+    return res
+
+if __name__ == '__main__':
+    n, m = (int(x) for x in input().split())
+    print(sum(fib(n, m)))
---
+++ python/wfmd.py
@@ -0,0 +1,44 @@
+import math
+
+cnk_values = {}
+
+def c(n, k):
+    if k == 0 or n < k:
+        return 0
+    if k == 1 or k == n:
+        return 1
+    if (n, k) in cnk_values:
+        return cnk_values[n, k]
+    result = (c(n - 1, k - 1) + c(n - 1, k))
+    cnk_values[n, k] = result
+    return result
+
+
+def prob(SIZE, p, k):
+    return c(SIZE, k) * math.pow(p, k) * math.pow(1 - p, SIZE - k)
+
+
+def gen(SIZE, gen):
+    res = [0] * SIZE
+    for i in range(SIZE):
+        p = 0
+        for j in range(len(gen)):
+            p += gen[j] * prob(SIZE, (j + 1) / SIZE, i + 1)
+        res[i] = p
+    return res
+
+if __name__ == '__main__':
+    N, m, g, k = (int(x) for x in input().split())
+    SIZE = 2 * N
+    generation = [0] * SIZE
+    generation[m - 1] = 1
+    for i in range(g):
+        print(' '.join(['%.3f' % x for x in generation]))
+        generation = gen(SIZE, generation)
+    print(' '.join(['%.3f' % x for x in generation]))
+
+    res = 0
+    for i in range(SIZE - k, SIZE):
+        res += generation[i]
+    print(1 - res)
+
