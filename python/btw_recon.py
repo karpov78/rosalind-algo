d6a98aff75bd3ce3c21ba4df590ca36aaaa09559
evgeny <karpov78@gmail.com>
2014-02-01 13:03:10 -0500

    q1

--- python/btw_recon.py
+++ python/btw_recon.py
@@ -1,4 +1,7 @@
 from bisect import bisect_left
+from collections import defaultdict
+
+alphabet = '$ACGT'
 
 
 def indexOf(a, x):
@@ -12,20 +15,17 @@ def indexOf(a, x):
 count_cache = {}
 
 
-def count(i, l, alphabet='$ACGT'):
+def count(i, l):
     global count_cache
     if i in count_cache:
         return count_cache[i]
     elif i == 0:
         return [0] * len(alphabet)
     else:
-        c = list(calc_count(i - 1, l, alphabet))
-        c[indexOf(alphabet, l[i - 1][0])] += 1
-        count_cache[i] = c
-        return c
+        return calc_count(i, l)
 
 
-def calc_count(i, l, alphabet='$ACGT'):
+def calc_count(i, l):
     global count_cache
     c = [0] * len(alphabet)
     start = 0
@@ -36,55 +36,120 @@ def calc_count(i, l, alphabet='$ACGT'):
     for j in xrange(start + 1, i + 1):
         sIndex = indexOf(alphabet, l[j - 1][0])
         c[sIndex] += 1
-        count_cache[j] = list(c)
+        if j % 100 == 0:
+            count_cache[j] = list(c)
     return c
 
 
 def index(s):
     occ = {}
+    res = []
     for i in xrange(len(s)):
         if s[i] in occ:
             j = occ[s[i]] + 1
         else:
             j = 1
         occ[s[i]] = j
-        s[i] = '%s%03d' % (s[i], j)
-    return s
+        res.append('%s%06d' % (s[i], j))
+    return res
 
 
-def bwmatch(firstOccurrence, l, p, alphabet='$ACGT'):
-    top = 0
-    bottom = len(l) - 1
-    while top <= bottom:
-        if len(p) > 0:
-            s = p[-1]
-            p = p[:-1]
-            sIndex = indexOf(alphabet, s)
-            top = firstOccurrence[sIndex] + count(top, l, alphabet)[sIndex]
-            bottom = firstOccurrence[sIndex] + count(bottom + 1, l, alphabet)[sIndex] - 1
-        else:
-            return bottom - top + 1
-    return 0
+class BWT:
+    def __init__(self, s):
+        self.lf = {}
+        self.locate = {}
+        self.index = 0
+        self.bwt = self.bwt_bucket(s, (i for i in xrange(len(s))), 1)
 
-s = raw_input()
-p = raw_input().split(' ')
-# with open('/Users/evgeny/Downloads/bwtMatch.txt') as f:
+        i_bwt = index(self.bwt)
+        first = sorted([c for c in i_bwt])
+        self.lf = {i: indexOf(first, i_bwt[i]) for i in xrange(len(s))}
+
+        self.occ = [-1] * len(alphabet)
+        for i in xrange(len(first)):
+            sIndex = indexOf(alphabet, first[i][0])
+            if self.occ[sIndex] == -1:
+                self.occ[sIndex] = i
+
+
+    def bwt_bucket(self, str, bucket, order):
+        d = defaultdict(list)
+        for i in bucket:
+            key = str[i:i + order]
+            d[key].append(i)
+        result = ''
+        for key, v in sorted(d.iteritems()):
+            if len(v) > 1:
+                result += self.bwt_bucket(str, v, order * 2)
+            else:
+                result += str[v[0] - 1]
+                #                if self.index % 10 == 0:
+                self.locate[self.index] = v[0]
+                self.index += 1
+        return result
+
+    def bwmatch(self, p):
+        top = 0
+        bottom = len(self.bwt) - 1
+        while top <= bottom:
+            if len(p) > 0:
+                s = p[-1]
+                p = p[:-1]
+                sIndex = indexOf(alphabet, s)
+                top = self.occ[sIndex] + count(top, self.bwt)[sIndex]
+                bottom = self.occ[sIndex] + count(bottom + 1, self.bwt)[sIndex] - 1
+            else:
+                return [self.locate_position(idx) for idx in xrange(top, bottom + 1)]
+                # return bottom - top + 1
+        return []
+
+    def locate_position(self, idx):
+        index_offset = 0;
+        while not idx in self.locate:
+            if self.bwt[idx] == '$':
+                return -1 * index_offset
+            index_offset -= 1
+            idx = self.lf[idx]
+        return self.locate[idx] + index_offset
+
+    def __str__(self):
+        return 'BWT: %s\nLocate: %s\nLF: %s' % (self.bwt, self.locate, self.lf)
+
+
+s = raw_input() + '$'
+patterns = raw_input().split(' ')
+d = int(raw_input())
+
+b = BWT(s)
+
+for p in patterns:
+    seeds = [p[i:i + d + 1] for i in xrange(len(p) - d)]
+    print seeds
+
+# res = []
+# while True:
+#     p = raw_input()
+#     if not p: break
+#     res += b.bwmatch(p)
+# res.sort()
+# print ' '.join([str(x) for x in res])
+
+# with open('/Users/evgeny/Downloads/patternMatch.txt', 'r') as f:
 #     f.readline()
-#     s = f.readline().strip()
-#     p = f.readline().strip().split(' ')
-#     f.readline()
+#     s = f.readline().strip() + '$'
+#     b = BWT(s)
+#     print 'BWT done'
+#
+#     res = []
+#     while True:
+#         p = f.readline().strip()
+#         if p == 'Output:':
+#             break
+#         m = b.bwmatch(p)
+#         for x in m:
+#             assert s[x:x + len(p)] == p, "Invalid match for %s (%d reported), but was %s" % (p, x, s[x:x + len(p)])
+#         res += m
+#     res.sort()
+#
 #     exp = [int(x) for x in f.readline().strip().split(' ')]
-
-alphabet = '$ACGT'
-f = [c for c in s]
-f.sort()
-firstOccurrence = [-1] * len(alphabet)
-for i in xrange(len(f)):
-    sIndex = indexOf(alphabet, f[i])
-    if firstOccurrence[sIndex] == -1:
-        firstOccurrence[sIndex] = i
-l = [c for c in s]
-
-res = [bwmatch(firstOccurrence, l, x, alphabet) for x in p]
-print ' '.join([str(x) for x in res])
-# assert exp == res, "Expected \n%s\n, but was \n%s" % (str(exp), str(res))
+#     assert exp == res, "Expected \n%s\n, but was \n%s" % (str(exp), str(res))
--- python/part_suff.py
+++ python/part_suff.py
@@ -23,9 +23,10 @@ def suffix_array_ManberMyers(str, k):
     return sort_bucket(str, (i for i in xrange(len(str))), 1, k)[0]
 
 
-s = raw_input()
-k = int(raw_input())
-# with open('/Users/evgeny/Downloads/partialSuffixArray.txt', 'r') as f:
+if __name__ == '__main__':
+    s = raw_input()
+    k = int(raw_input())
+    # with open('/Users/evgeny/Downloads/partialSuffixArray.txt', 'r') as f:
 #     f.readline()
 #     s = f.readline().strip()
 #     k = int(f.readline().strip())
@@ -36,10 +37,10 @@ k = int(raw_input())
 #         if not l: break
 #         exp.append(l.strip())
 
-sa = suffix_array_ManberMyers(s, k)
-res = []
-for suff in sa:
-    res.append('%d,%d' % suff)
-print '\n'.join(res)
-# assert exp == res
+    sa = suffix_array_ManberMyers(s, k)
+    res = []
+    for suff in sa:
+        res.append('%d,%d' % suff)
+    print '\n'.join(res)
+#   assert exp == res
 
